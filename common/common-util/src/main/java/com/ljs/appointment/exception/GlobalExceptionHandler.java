4602b8b67d7c3cb81f755e08e411512a01042449
ljs <1791749970@qq.com>
2021-06-11 14:50:45 +0800

    add globalexcptionHanlder

---
+++ common/common-util/src/main/java/com/ljs/appointment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.ljs.appointment.exception;
+
+
+import com.ljs.appointment.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理类
+ *
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+
+}
+
