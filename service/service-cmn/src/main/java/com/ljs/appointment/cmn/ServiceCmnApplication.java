7c94117092b6c29c3a88da49d7d76f7fd601db9e
ljs <1791749970@qq.com>
2021-06-13 15:42:29 +0800

    update controller update yml -> password   redis.host

--- service/service-cmn/src/main/java/com/ljs/appointment/cmn/ServiceCmnApplication.java
+++ service/service-cmn/src/main/java/com/ljs/appointment/cmn/ServiceCmnApplication.java
@@ -2,8 +2,10 @@ package com.ljs.appointment.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ljs")
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
--- service/service-cmn/src/main/java/com/ljs/appointment/cmn/controller/DictController.java
+++ service/service-cmn/src/main/java/com/ljs/appointment/cmn/controller/DictController.java
@@ -3,6 +3,8 @@ package com.ljs.appointment.cmn.controller;
 import com.ljs.appointment.cmn.service.DictService;
 import com.ljs.appointment.model.cmn.Dict;
 import com.ljs.appointment.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import java.util.List;
  * @author ljs
  * @create 2021-3-13
  */
+@Api("数据字典接口")
 @RestController
 @RequestMapping("/admin/cmn/dict")
 public class DictController {
@@ -22,6 +25,7 @@ public class DictController {
     @Autowired
     private DictService dictService;
 
+    @ApiOperation(value = "根据数据id查询子数据列表")
     @GetMapping("/findchildData/{id}")
     public Result findChildData(@PathVariable("id") Long id) {
         List<Dict> list = dictService.findChildData(id);
