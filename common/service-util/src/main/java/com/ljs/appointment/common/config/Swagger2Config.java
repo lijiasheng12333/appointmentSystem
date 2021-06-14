78af3f28a25ae6859babaaaeb9a5a98eb12a1b24
ljs <1791749970@qq.com>
2021-06-14 22:08:28 +0800

    delete directory Swagger2Config

--- common/service-util/src/main/java/com/ljs/appointment/common/config/Swagger2Config.java
+++ common/service-util/src/main/java/com/ljs/appointment/common/config/Swagger2Config.java
@@ -1,4 +1,4 @@
-package com.ljs.appointment.common.config.Swagger2Config;
+package com.ljs.appointment.common.config;
 
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
